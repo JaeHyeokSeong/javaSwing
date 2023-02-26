e89d04a257b67ac0d0bfed9103677cbb6c0662e7
JaeHyeokSeong <seong5763@gmail.com>
2023-02-27 01:08:47 +1100

    implement menu bar in the working page

--- JavaSwingTutorial/src/Project1/WorkingPage.java
+++ JavaSwingTutorial/src/Project1/WorkingPage.java
@@ -2,6 +2,8 @@ package Project1;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class WorkingPage extends JFrame {
 
@@ -23,7 +25,44 @@ public class WorkingPage extends JFrame {
 
         add(welcomeLable);
 
+        // implements menu bar
+        setJMenuBar(setMenuBar());
+
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setVisible(true);
+    }
+    private JMenuBar setMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        // file menu
+        JMenu fileMenu = new JMenu("File");
+        // file menu items
+        JMenuItem openMenuItem = new JMenuItem("Open");
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+        // add menu items
+        fileMenu.add(openMenuItem);
+        fileMenu.addSeparator();
+        fileMenu.add(exitMenuItem);
+        // file menu items events
+        openMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("open menu item has clicked");
+            }
+        });
+
+        exitMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("exit menu item has clicked");
+                System.exit(0);
+            }
+        });
+        // about menu
+        JMenu aboutMenu = new JMenu("About");
+
+        menuBar.add(fileMenu);
+        menuBar.add(aboutMenu);
+
+        return menuBar;
     }
 }
