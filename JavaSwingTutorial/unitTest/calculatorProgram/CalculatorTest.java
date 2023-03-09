cff55c2044ed45be6c22dcff21f0c7a11b7b5be6
JaeHyeokSeong <seong5763@gmail.com>
2023-03-10 01:25:26 +1100

    java unit test practise

---
+++ JavaSwingTutorial/unitTest/calculatorProgram/CalculatorTest.java
@@ -0,0 +1,28 @@
+package calculatorProgram;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        assertEquals(10, Calculator.add(5, 5));
+    }
+
+    @Test
+    void minus() {
+        assertEquals(5, Calculator.minus(10, 5));
+    }
+
+    @Test
+    void multiply() {
+        assertEquals(100, Calculator.multiply(10, 10));
+    }
+
+    @Test
+    void divide() {
+        assertEquals(2, Calculator.divide(10, 5));
+    }
+}
